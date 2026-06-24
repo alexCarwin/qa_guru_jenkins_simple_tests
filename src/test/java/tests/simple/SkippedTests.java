cf6c609f67df79da85167b277c4b7796976f2cce
Alexey Latushkin <alexeilatushkin@yandex.ru>
2026-06-24 20:40:39 +0300

    added simple assert tests

---
+++ src/test/java/tests/simple/SkippedTests.java
@@ -0,0 +1,18 @@
+package tests.simple;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SkippedTests {
+
+    @Test
+    @Disabled("Some reason")
+    void someTest() { assertTrue(false);}
+
+    @Test
+    @Disabled("Some reason")
+    void someTest1() { assertTrue(false);}
+
+}

83efedbcd161e85b4f1ebd7679e408d05c30f2b9
Alexey Latushkin <alexeilatushkin@yandex.ru>
2026-07-08 22:07:46 +0300

    refactoring

--- src/test/java/tests/simple/TestBase.java
+++ src/test/java/tests/simple/TestBase.java
@@ -46,11 +46,7 @@ public class TestBase {
         Attach.browserConsoleLogs();
         Attach.addVideo();
 //        Attach.attachAsText("File","Content");
-
-    }
-
-    @AfterEach
-    void tearDown() {
         closeWebDriver();
     }
+
 }

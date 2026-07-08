c6a1e0a12f8f645361c2083b5ac06e4269d2667c
Alexey Latushkin <alexeilatushkin@yandex.ru>
2026-07-08 22:12:49 +0300

    added //

--- src/test/java/tests/simple/SelenideTest.java
+++ src/test/java/tests/simple/SelenideTest.java
@@ -24,35 +24,35 @@ public class SelenideTest {
 //        Configuration.browserVersion = "130.0";
 //        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 }
-    @Test
-    public void testGitIgnoreSearch(){
-        open("https://github.com/");
-        $(".header-search-button").click();
-        $("#query-builder-test").sendKeys("eroshenkoam/allure-example");
-        $("#query-builder-test").submit();
-        $(linkText("eroshenkoam/allure-example")).click();
-        $(withText(".gitignore")).should(Condition.exist);
-    }
-
-    @Test
-    public void testGitIgnoreSearchFailed(){
-
-        open("https://github.com/");
-
-        $(".header-search-button").click();
-        $("#query-builder-test").sendKeys("eroshenkoam/allure-example");
-        $("#query-builder-test").submit();
-        $(linkText("eroshenkoam/allure-example")).click();
-        $(withText(".gitignor1")).should(Condition.exist);
-    }
-
-    @Test
-    public void testGitIgnoreSearchNotFullyText(){
-        open("https://github.com/");
-        $(".header-search-button").click();
-        $("#query-builder-test").sendKeys("eroshenkoam/allure-example");
-        $("#query-builder-test").submit();
-        $(linkText("eroshenkoam/allure-example")).click();
-        $(withText(".gitignor")).should(Condition.exist);
-    }
+//    @Test
+//    public void testGitIgnoreSearch(){
+//        open("https://github.com/");
+//        $(".header-search-button").click();
+//        $("#query-builder-test").sendKeys("eroshenkoam/allure-example");
+//        $("#query-builder-test").submit();
+//        $(linkText("eroshenkoam/allure-example")).click();
+//        $(withText(".gitignore")).should(Condition.exist);
+//    }
+//
+//    @Test
+//    public void testGitIgnoreSearchFailed(){
+//
+//        open("https://github.com/");
+//
+//        $(".header-search-button").click();
+//        $("#query-builder-test").sendKeys("eroshenkoam/allure-example");
+//        $("#query-builder-test").submit();
+//        $(linkText("eroshenkoam/allure-example")).click();
+//        $(withText(".gitignor1")).should(Condition.exist);
+//    }
+//
+//    @Test
+//    public void testGitIgnoreSearchNotFullyText(){
+//        open("https://github.com/");
+//        $(".header-search-button").click();
+//        $("#query-builder-test").sendKeys("eroshenkoam/allure-example");
+//        $("#query-builder-test").submit();
+//        $(linkText("eroshenkoam/allure-example")).click();
+//        $(withText(".gitignor")).should(Condition.exist);
+//    }
 }
